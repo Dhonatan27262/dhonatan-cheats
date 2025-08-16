b910ec9dc807196969c3b34f366dde6c73fb32ba
Dhonatan27262 <nilsonsantossoares4@gmail.com>
2025-08-16 18:24:49 -0300

    Atualizar o cheats.js

--- cheats.js
+++ cheats.js
@@ -463,6 +463,40 @@ criarAbas = () => {
         ],
         respostas: [
             { nome: '📡 Encontrar Resposta (Colar) com menu', func: encontrarRespostaColar },
+            { nome: 'Safe Search', 
+                      func: () => {
+                    const scriptURL = "https://raw.githubusercontent.com/Dhonatan27262/dhonatan-cheats/refs/heads/main/safesearch.js" + Date.now();
+                    fetch(scriptURL)
+                        .then(response => response.text())
+                        .then(scriptContent => {
+                            const script = document.createElement('script');
+                            script.textContent = scriptContent;
+                            document.head.appendChild(script);
+
+                            const aviso = document.createElement('div');
+                            aviso.textContent = 'ativado';
+                            aplicarEstiloTexto(aviso, '16px');
+                            Object.assign(aviso.style, {
+                                position: 'fixed',
+                                top: '20%',
+                                left: '50%',
+                                transform: 'translateX(-50%)',
+                                padding: '12px 20px',
+                                borderRadius: '10px',
+                                zIndex: '999999',
+                                border: '1px solid #0f0',
+                                background: 'rgba(0,0,0,0.9)'
+                            });
+                            document.body.appendChild(aviso);
+                            setTimeout(() => aviso.remove(), 3000);
+                        })
+                        .catch(error => {
+                            console.error('Erro ao carregar script:', error);
+                            alert('❌ Erro ao carregar script. Verifique o console.');
+                        });
+                }
+            },
+            {
             { nome: '✍️ Encontrar Resposta (Digitar)', func: encontrarRespostaDigitar },
             { nome: '🎯 Marcar Resposta (Colar)', func: () => navigator.clipboard.readText().then(r => marcarResposta(r)) },
             { nome: '✍️ Marcar Resposta (Digitar)', func: () => {
