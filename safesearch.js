9dc379577aa4ec2c338d584668bcfb87d2800c1a
Dhonatan27262 <nilsonsantossoares4@gmail.com>
2025-08-16 18:43:04 -0300

    Atualizar o safesearch.js

--- safesearch.js
+++ safesearch.js
@@ -1,3 +1,12 @@
-javascript:(function(){
-    window.location.href = "https://www.google.com/preferences#safe=active";
+(function(){
+    // Pega a pesquisa atual (se houver) ou pergunta ao usuário
+    let query = prompt("Digite o que deseja pesquisar (SafeSearch ON):", "");
+    
+    if(query){
+        // Redireciona com SafeSearch ativado
+        window.location.href = "https://www.google.com/search?q=" + encodeURIComponent(query) + "&safe=active";
+    } else {
+        // Se não digitar nada, abre só o Google com SafeSearch
+        window.location.href = "https://www.google.com/?safe=active";
+    }
 })();
