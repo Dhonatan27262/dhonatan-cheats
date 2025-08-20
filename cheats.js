0fba98ee98357d3cca0e9c0fcfb1f4f58019a3f6
Dhonatan27262 <nilsonsantossoares4@gmail.com>
2025-08-20 12:02:54 -0300

    Atualizar o cheats.js

--- cheats.js
+++ cheats.js
@@ -699,7 +699,12 @@ const criarInterface = () => {
         backgroundColor: 'rgba(0,0,0,0.85)', zIndex: '999999',
         display: 'flex', alignItems: 'center', justifyContent: 'center'
     });
-    if (typeof sendToast === 'function') sendToast("👋 Bem-vindo!", 3000);
+    
+       // Mostra aviso ao abrir o menu
+    if (typeof sendToast === 'function') {
+        sendToast("👋 Bem-vindo!", 3000, "top");
+    }
+};
 
     janela = document.createElement('div');
     aplicarEstiloContainer(janela);
