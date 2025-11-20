121d04ea9fee5012455bcdc45b69252778017aa3
painelaux <nilsonsantossoares4@gmail.com>
2025-11-20 11:56:40 -0300

    Atualizar o senhas.js

--- senhas.js
+++ senhas.js
@@ -4,7 +4,6 @@ window.verificarSenha = function(senha) {
         "tainara",
         "rick",
         "013179",
-        "Gabrielhirata",//20do11
         "FLASH1K_",//22do11
         "carlosdantas2007",//26do11
         "lacerda22"//29d11
