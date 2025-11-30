672ae792a0107a8432f4616ba91b4156685bb5b8
painelaux <nilsonsantossoares4@gmail.com>
2025-11-30 19:40:17 -0300

    Atualizar o cheats.js

--- cheats.js
+++ cheats.js
@@ -1334,7 +1334,7 @@ const criarMenu = () => {
 
     // Criar e configurar a imagem do gorro
     const gorroImg = document.createElement('img');
-    gorroImg.src = 'https://cdn.jsdelivr.net/gh/auxpainel/2050@main/fotocanto.js';
+    gorroImg.src = 'https://cdn.jsdelivr.net/gh/auxpainel/2050@main/Natal.png';
     gorroImg.alt = 'Gorro de Natal';
     Object.assign(gorroImg.style, {
         width: '36px',
