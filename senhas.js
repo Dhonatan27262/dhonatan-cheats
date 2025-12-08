77f699ff889eab1d471f27d87b80905a18993f62
painelaux <nilsonsantossoares4@gmail.com>
2025-12-08 10:35:07 -0300

    Atualizar o senhas.js

--- senhas.js
+++ senhas.js
@@ -3,9 +3,7 @@ window.verificarSenha = function(senha) {
     const senhasValidas = [
         "tainara",
         "013179",
-        "juregps2",//8do12
-        "1911",//vitalicio
-        "A10121605"//8do12
+        "1911"//vitalicio
 ];
     // Verifica a senha exatamente como digitada (case sensitive)
     return senhasValidas.includes(senha);
